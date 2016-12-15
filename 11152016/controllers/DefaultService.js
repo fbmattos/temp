c11913819c655d232758d2bea10c72e33d926e2d
fbmattos <fbmattos@gmail.com>
2016-12-15 22:47:35 +0000

    Generated code by SwaggerHub

--- 11152016/controllers/DefaultService.js
+++ 11152016/controllers/DefaultService.js
@@ -26,6 +26,29 @@ exports.climate_state = function(args, res, next) {
   
 }
 
+exports.drive_state = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * id (String)
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "shift_state" : "sport",
+  "heading" : 6,
+  "latitude" : 37.4292,
+  "speed" : 94.5,
+  "longitude" : 122.1381
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
 exports.set_temps = function(args, res, next) {
   /**
    * parameters expected in the args:
@@ -58,29 +81,6 @@ exports.vehicleGET = function(args, res, next) {
   "vin" : "5YJSA1CN5CFP01657",
   "display_name" : "Fernando's Model S",
   "vehicle_id" : "1234567890"
-};
-  if(Object.keys(examples).length > 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  }
-  else {
-    res.end();
-  }
-  
-}
-
-exports.vehiclesIdData_requestDrive_stateGET = function(args, res, next) {
-  /**
-   * parameters expected in the args:
-  * id (String)
-  **/
-    var examples = {};
-  examples['application/json'] = {
-  "shift_state" : "sport",
-  "heading" : 6,
-  "latitude" : 37.4292,
-  "speed" : 94.5,
-  "longitude" : 122.1381
 };
   if(Object.keys(examples).length > 0) {
     res.setHeader('Content-Type', 'application/json');
