a45e12c9009c90ef2df00dd62f44b38430328874
fbmattos <fbmattos@gmail.com>
2016-12-15 22:46:57 +0000

    Generated code by SwaggerHub

--- 11152016/controllers/Default.js
+++ 11152016/controllers/Default.js
@@ -10,14 +10,14 @@ module.exports.climate_state = function climate_state (req, res, next) {
   Default.climate_state(req.swagger.params, res, next);
 };
 
-module.exports.drive_state = function drive_state (req, res, next) {
-  Default.drive_state(req.swagger.params, res, next);
-};
-
 module.exports.set_temps = function set_temps (req, res, next) {
   Default.set_temps(req.swagger.params, res, next);
 };
 
 module.exports.vehicleGET = function vehicleGET (req, res, next) {
   Default.vehicleGET(req.swagger.params, res, next);
+};
+
+module.exports.vehiclesIdData_requestDrive_stateGET = function vehiclesIdData_requestDrive_stateGET (req, res, next) {
+  Default.vehiclesIdData_requestDrive_stateGET(req.swagger.params, res, next);
 };
